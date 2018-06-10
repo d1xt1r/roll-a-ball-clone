fd53dcd5968463ca0e97cb6a1db473f64d1137ca
Denislav Georgiev <d1xt1r@gmail.com>
2018-06-10 17:02:35 +0300

    Add camera movement script

---
+++ Roll a Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset; // The distance between the Player and the Camera
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = player.transform.position + offset; // Moves the camera to new position aligned with the player
+	}
+}
